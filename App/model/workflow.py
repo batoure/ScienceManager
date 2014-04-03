a3321b44629e53d467c4e7a73ff4717392c541bf
Batoure <austin@digitalminion.com>
2014-04-03 10:20:30 -0400

    added models to workflow

--- App/model/workflow.py
+++ App/model/workflow.py
@@ -2,8 +2,21 @@
 
 
 class Workflow(object):
-    """ A model for configuration data """
+    def __init__(self):
+        self.id = None
+        self.name = None
+        self.tasks = {}
+        self.batch_id = None
+
+
+class Tasks(object):
+    def __init__(self):
+        self.task_num = None
+        self.action = Action()
+
+
+class Action(object):
     def __init__(self):
         self.name = None
-        self.program = None
-        self.database = None
+        self.type_id = None
+        self.text = None
